de4a890d585517a41a137b9fe73136d38dd8baae
Umair Akbar <umairakbar5665@gmail.com>
2026-01-22 16:19:59 +0500

    wrote initdatabase

--- lib/db_handler.dart
+++ lib/db_handler.dart
@@ -1,1 +1,14 @@
+import 'package:sqflite/sqflite.dart';
 
+class DbHelper {
+  static Database? _db;
+  Future<Database?> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initdatabase();
+    return _db;
+  }
+
+  initdatabase() async {}
+}
