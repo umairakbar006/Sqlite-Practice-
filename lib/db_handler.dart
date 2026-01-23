89e987f39051570484bc8501910d51fbe14d4734
Umair Akbar <umairakbar5665@gmail.com>
2026-01-23 14:50:33 +0500

    Create the Database

--- lib/db_handler.dart
+++ lib/db_handler.dart
@@ -1,4 +1,8 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'notes.dart';
+import 'package:path/path.dart';
+import 'dart:io' as io;
 
 class DbHelper {
   static Database? _db;
@@ -10,5 +14,22 @@ class DbHelper {
     return _db;
   }
 
-  initdatabase() async {}
+  initdatabase() async {
+    io.Directory documentDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentDirectory.path, 'notes.db');
+    var db = await openDatabase(path, version: 1, onCreate: _oncreate);
+    return db;
+  }
+
+  _oncreate(Database db, int version) async {
+    await db.execute(
+      "CREATE TABLE notes(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, age INTEGER NOT NULL, description TEXT NOT NULL, email TEXT NOT NULL)",
+    );
+  }
+
+  Future<NotesModel> insert(NotesModel notesModel) async {
+    var dbClient = await db;
+    await dbClient!.insert('notes', notesModel.toMap());
+    return notesModel;
+  }
 }
