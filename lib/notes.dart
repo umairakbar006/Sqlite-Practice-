109e38e3efd292acbccf597fe04395187400e6c7
Umair Akbar <umairakbar5665@gmail.com>
2026-02-04 16:09:18 +0500

    Made code more readable

--- lib/notes.dart
+++ lib/notes.dart
@@ -4,17 +4,16 @@ class NotesModel {
   final String? description;
 
   NotesModel({this.id, required this.title, required this.description});
-
-  //This is method to save data into database
-
-  Map<String, Object?> toMap() {
-    return {'id': id, 'title': title, 'description': description};
-  }
-
   // This method is used to read data from database
 
   NotesModel.fromMap(Map<String, dynamic> res)
     : id = res['id'],
       title = res['title'],
       description = res['description'];
+
+  //This is method to save data into database
+
+  Map<String, Object?> toMap() {
+    return {'id': id, 'title': title, 'description': description};
+  }
 }
